9a1bc8105c08df9813ef686d08610eaf783aa623
Martinpanzy <36052046+Martinpanzy@users.noreply.github.com>
2019-05-17 22:50:57 +0100

    Add files via upload

--- shoe_detection/src/norm.py
+++ shoe_detection/src/norm.py
@@ -5,24 +5,42 @@ import sensor_msgs.point_cloud2 as pc2
 from sensor_msgs.msg import Image, PointCloud2
 import tf
 import pcl
+import ros_numpy
 
 def depth_callback(cloud):
-	ne = cloud.make_IntegralImageNormalEstimation()
-	print ('set_NormalEstimation_Method_AVERAGE_3D_GRADIENT: ')
-	ne.set_NormalEstimation_Method_AVERAGE_3D_GRADIENT ()
-	print ('set_MaxDepthChange_Factor: ')
-	ne.set_MaxDepthChange_Factor(0.02)
-	print ('set_NormalSmoothingSize: ')
-	ne.set_NormalSmoothingSize(10.0)
-	print ('set OK')
-	print ('compute2 - start')
-	normals = ne.compute2(cloud)
-	print ('compute2 - end')
-	print (str(normals.size))
+	'''	
+	pc = pc2.read_points(cloud, field_names = ('x', 'y', 'z'), skip_nans = True)
+	pc_list = []
+	for p in pc:
+		pc_list.append([p[0], p[1], p[2]])
+	p = pcl.PointCloud()
+	p.from_list(pc_list)
+	'''
+
+	#pc = ros_numpy.numpify(cloud)
+	#pts = np.zeros((pc.shape[0],3))
+	#print(np.shape(pc))
+	#pts[:,0] = pc['x']
+	#pts[:,1] = pc['y']
+	#pts[:,2] = pc['z']
+	
+	pts = ros_numpy.point_cloud2.pointcloud2_to_xyz_array(cloud)	
+	p = pcl.PointCloud(np.array(pts, dtype = np.float32))
+
+	seg = p.make_segmenter_normals(ksearch=50)
+	seg.set_optimize_coefficients(True)
+	seg.set_model_type(pcl.SACMODEL_NORMAL_PLANE)
+	seg.set_method_type(pcl.SAC_RANSAC)
+	seg.set_distance_threshold(0.01)
+	seg.set_normal_distance_weight(0.01)
+	seg.set_max_iterations(100)
+	indices, coefficients = seg.segment()
+	print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+	print('Model coefficients: ' + str(coefficients[0]) + ' ' + str(coefficients[1]) + ' ' + str(coefficients[2]) + ' ' + str(coefficients[3]))
 
 def listener():
 	rospy.init_node("normalnormal", anonymous=True)
-	rospy.Subscriber('/camera/depth_registered/points', p, depth_callback)
+	rospy.Subscriber('/camera/depth_registered/points', PointCloud2, depth_callback)
 	rospy.spin()
 if __name__ == '__main__':
 	listener()
